cfee537af8653a7124074ee31fc96b99ef888210
Sanjay Purswani <sanjsanj@hotmail.com>
2019-01-27 18:21:09 +0000

    Get in code highlighting

--- src/components/CodeKampala/index.js
+++ src/components/CodeKampala/index.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import 'prismjs'
+import 'prismjs/themes/prism-tomorrow.css'
+import PrismCode from 'react-prism'
 
 import { StyledSection } from '../Styles'
 
@@ -6,13 +9,13 @@ export default function CodeKampala() {
   return (
     <StyledSection>
       <h2>code:KAMPALA</h2>
-      <p>
+      <PrismCode className="language-javascript">
         code:KAMPALA is part of code:AFRICA which is a social enterprise
         focusing on training young women in Sub Saharan Africa in code, and
         equipping them with the skills to find jobs in software development and
         STEM careers. Our first market is Uganda, which has one of the highest
         youth unemployment rates in the world.
-      </p>
+      </PrismCode>
       <p>Watch this space... more information coming soon!</p>
     </StyledSection>
   )
