24cd5cdcd165545104a5aaf33041c57b4ab6662b
Sanjay Purswani <sanjsanj@hotmail.com>
2019-01-27 15:00:08 +0000

    Put in code:africa config sets

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './header'
+import Header from '../header'
 import './layout.css'
 
 const Layout = ({ children }) => (
@@ -29,9 +29,7 @@ const Layout = ({ children }) => (
         >
           {children}
           <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+            © {new Date().getFullYear()}
           </footer>
         </div>
       </>
--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,8 @@ import SEO from '../components/seo'
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h2>NOT FOUND</h2>
+    <p>Oopsy!  You just found a page that doesn't exist.</p>
   </Layout>
 )
 
